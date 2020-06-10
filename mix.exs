9ca7b208551f2b7560aadb04993a228dcd265fbd
Paul Schoenfelder <paulschoenfelder@fastmail.com>
2020-06-10 10:51:30 -0400

    release: version 0.1.4

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Picosat.MixProject do
   def project do
     [
       app: :picosat_elixir,
-      version: "0.1.3",
+      version: "0.1.4",
       elixir: "~> 1.7",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
